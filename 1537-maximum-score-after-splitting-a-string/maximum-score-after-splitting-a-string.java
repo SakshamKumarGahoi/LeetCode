3565afcffe566896e0669f4c4b18e7285a891699
Saksham Kumar Gahoi <143262344+SakshamKumarGahoi@users.noreply.github.com>
2025-06-24 01:21:06 +0530

    Time: 1 ms (99.35%) | Memory: 41.4 MB (89.96%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxScore(String s) {
+        int totalOnes = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') totalOnes++;
+        }
+
+        int maxScore = 0;
+        int leftZeros = 0;
+        int leftOnes = 0;
+
+        // Go only till second last character (split should be non-empty on both sides)
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '0') {
+                leftZeros++;
+            } else {
+                leftOnes++;
+            }
+            int score = leftZeros + (totalOnes - leftOnes);
+            maxScore = Math.max(maxScore, score);
+        }
+
+        return maxScore;
+    
+        
+    }
+}
